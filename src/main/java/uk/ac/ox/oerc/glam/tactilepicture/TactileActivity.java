775547e5b7e2249cea70a96ec221f950b867df47
iaine <iain.emsley@kellogg.ox.ac.uk>
2017-08-03 21:15:23 +0100

    Removing Gesture from Activity

--- src/main/java/uk/ac/ox/oerc/glam/tactilepicture/TactileActivity.java
+++ src/main/java/uk/ac/ox/oerc/glam/tactilepicture/TactileActivity.java
@@ -1,18 +1,22 @@
 package uk.ac.ox.oerc.glam.tactilepicture;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 
-public class TactileActivity extends AppCompatActivity {
+public class TactileActivity extends AppCompatActivity  {
 
-
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new TactileView(this, null));
     }
-
 
 }
