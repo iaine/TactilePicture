a68a39054eb6b74a1475f2ac3ae180fa72479c92
iaine <iain.emsley@kellogg.ox.ac.uk>
2017-08-03 21:23:11 +0100

    DAO Tests

---
+++ src/test/java/uk/ac/ox/oerc/glam/tactilepicture/TactileDAOTests.java
@@ -0,0 +1,45 @@
+package uk.ac.ox.oerc.glam.tactilepicture;
+
+import android.graphics.PointF;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for the DAO
+ */
+
+public class TactileDAOTests {
+
+    @Test
+    public void testDAOFileExists() {
+        TactileDAO td = new TactileDAO();
+        PointF point = new PointF(750, 1092);
+        String audioFile = td.getAudio(point);
+        assert(audioFile == "ladder.mp3");
+    }
+
+    @Test
+    public void testDAOFileNotExists() {
+        TactileDAO td = new TactileDAO();
+        PointF point = new PointF(400, 1092);
+        String audioFile = td.getAudio(point);
+        assert(audioFile == "");
+    }
+
+    @Test
+    public void testDAOFileStopTrue() {
+        TactileDAO td = new TactileDAO();
+        PointF point = new PointF(76, 220);
+        boolean stop = td.getStop(point);
+        assertTrue(stop);
+    }
+
+    @Test
+    public void testDAOFileStopFalse() {
+        TactileDAO td = new TactileDAO();
+        PointF point = new PointF(50, 220);
+        boolean stop = td.getStop(point);
+        assertTrue(stop);
+    }
+}
